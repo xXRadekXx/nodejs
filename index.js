d3ca3720ac7a489d64c2d41bf7638f783a3dc6cb
xXRadekXx <dvorra.chodovka@seznam.cz>
2023-11-22 12:09:15 +0100

    ds

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')//import express fw
+const app = express()//spusteni expresu
+const port = 80//definovani portu
+const path = require('path');//pro manipulaci s cestami, ať už se jedná o absolutní cesty, relativní cesty
+const bodyParser = require('body-parser');//imort bodyParseru
+
+app.use(bodyParser.urlencoded({ extended: false })); //dekoduje data poslana pres POST
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+var mysql = require('mysql2');
+
+const con = mysql.createConnection({
+    host: '192.168.1.161', // Název nebo IP adresa serveru databáze
+    user: 'radek.dvorak', // Uživatelské jméno
+    password: 'Dvo240512Rak6057', // Heslo
+    database: 'radek.dvorak', // Název databáze
+    port: 3001
+  });
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get('/createuser', (req, res) => {//home routa
+    
+
+  
+  res.render('newuser');
+
+})
+
+app.post('/createuser', function (request, response, next) {
+  console.log(request.body)
+    // SQL dotaz pro vložení dat do databáze
+    var sql = `INSERT INTO rawr (fname, lname, age) VALUES ('${request.body.fname}', '${request.body.lname}','${request.body.age}')`;
+   
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(results);
+    })
+    response.send(`Uživatele byli vloženi do DB`)
+   
+  })
+app.get('/', (req, res) => {//home routa
+    
+
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM rawr", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+          res.render('index', { result });
+        });
+      });
+
+})
+app.listen(port, () => {//spustni serveru
+  console.log(`Example app listening on port ${port}`)
+})
+
